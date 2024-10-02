61484237dd5cdeb0a2445d34ec858ce7dddb43c3
Amit Shaw <iamitshaw.101@gmail.com>
2024-10-02 16:12:09 +0530

    fix(config): ts-node wasn't compiling typescript

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,11 @@
 const userName = "Amit Shaw";
 console.error(`Hello ${userName}!`);
+
+/* function to generate table */
+function table(tableOf: number = 2): undefined {
+    for (let index = 1; index <= 10; index++) {
+        console.error(`${tableOf} * ${index} = ${tableOf * index}`);
+    }
+}
+
+table(47);
