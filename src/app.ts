755c967a76db82d0255f58524d8e0ccd3a7ae04e
Amit Shaw <iamitshaw.101@gmail.com>
2024-10-04 08:44:47 +0530

    feature(config): helmet setup

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import usersRoute from "./routes/users.route";
 import { globalErrorHandler } from "./middlewares/global-error-handler.middleware";
 import { routeNotFoundHandler } from "./utils/404_not_found.util";
 import healthsRouter from "./routes/healths.route";
+import helmet from "helmet";
 
 const app: Application = express();
+
+/* third party Niddleware */
+app.use(helmet());
 
 /* BuiltIn Middleware */
 app.use(express.json({ limit: 100 }));
